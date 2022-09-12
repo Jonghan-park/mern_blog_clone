ed4ae06e0274e89be04e9506def470120802d44d
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-12 17:51:45 -0600

    Update mui

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <AppBar>
+      <Toolbar>
+        <Typography>BlogsApp</Typography>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
