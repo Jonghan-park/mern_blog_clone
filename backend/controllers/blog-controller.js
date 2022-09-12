e6f6b159d5c0662737c34c799cee2175cb5631f1
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-12 10:49:43 -0600

    Update delete function and change blog type

--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -28,3 +28,46 @@ exports.addBlog = async (req, res, next) => {
   }
   return res.status(200).json({ blog });
 };
+
+exports.updateBlog = async (req, res, next) => {
+  const { title, desc } = req.body;
+  const blogId = req.params.id;
+  let blog;
+  try {
+    blog = await Blog.findByIdAndUpdate(blogId, { title, desc });
+  } catch (error) {
+    return console.log(error);
+  }
+  if (!blog) {
+    return res.status(500).json({ message: "Unable To Update The Blog" });
+  }
+  return res.status(200).json({ blog });
+};
+
+exports.getById = async (req, res, next) => {
+  const id = req.params.id;
+  let blog;
+  try {
+    blog = await Blog.findById(id);
+  } catch (error) {
+    return console.log(error);
+  }
+  if (!blog) {
+    return res.status(404).json({ message: "No Blog Found" });
+  }
+  return res.status(200).json({ blog });
+};
+
+exports.deleteBlog = async (req, res, next) => {
+  const id = req.params.id;
+  let blog;
+  try {
+    blog = await Blog.findByIdAndRemove(id);
+  } catch (error) {
+    return console.log(error);
+  }
+  if (!blog) {
+    return res.status(400).json({ message: "Unable To Delete" });
+  }
+  return res.status(202).json({ message: "Successfully delete" });
+};
--- backend/routes/blog-routes.js
+++ backend/routes/blog-routes.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const blogRouter = express.Router();
-const { getAllBlogs } = require("../controllers/blog-controller");
+const {
+  getAllBlogs,
+  addBlog,
+  updateBlog,
+  getById,
+  deleteBlog,
+} = require("../controllers/blog-controller");
 
 blogRouter.get("/", getAllBlogs);
 blogRouter.post("/add", addBlog);
+blogRouter.put("/update/:id", updateBlog);
+blogRouter.get("/:id", getById);
+blogRouter.delete("/:id", deleteBlog);
 
 module.exports = blogRouter;
