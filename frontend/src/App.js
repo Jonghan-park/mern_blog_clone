d213509ded5a021b32b8e9f5c7258555d661b0c5
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-12 18:07:34 -0600

    Update router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 
 function App() {
-  return <Header />;
+  return (
+    <React.Fragment>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/"></Route>
+        </Routes>
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default App;
