06c696d660c702a137fcf6af86eeb1fc57546675
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-12 18:01:45 -0600

    Update navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+  return <Header />;
 }
 
 export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,32 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography, Box, Button } from "@mui/material";
 
 const Header = () => {
   return (
-    <AppBar>
+    <AppBar
+      sx={{
+        background:
+          "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 48%, rgba(0,212,255,1) 100%)",
+      }}
+    >
       <Toolbar>
-        <Typography>BlogsApp</Typography>
+        <Typography variant="h4">BlogsApp</Typography>
+        <Box display="flex" marginLeft="auto">
+          <Button
+            variant="contained"
+            sx={{ margin: 1, borderRadius: 10 }}
+            color="warning"
+          >
+            Login
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ margin: 1, borderRadius: 10 }}
+            color="warning"
+          >
+            SignUp
+          </Button>
+        </Box>
       </Toolbar>
     </AppBar>
   );
