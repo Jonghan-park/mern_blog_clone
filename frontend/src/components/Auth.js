f50ba084b5e333e10c75e26c41070a3854c3f20d
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-14 21:38:02 -0600

    Update auth

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,7 +1,64 @@
-import React from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [isSignup, setIsSignup] = useState(false);
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  return (
+    <div>
+      <form>
+        <Box
+          maxWidth={400}
+          display="flex"
+          flexDirection={"column"}
+          alignItems="center"
+          justifyContent={"center"}
+          boxShadow="10px 10px 20px #ccc"
+          padding={3}
+          margin="auto"
+          marginTop={5}
+          borderRadius={5}
+        >
+          <Typography variant="h2" padding={3} textAlign="center">
+            {!isSignup ? "Login" : "Signup"}
+          </Typography>
+
+          {isSignup && (
+            <TextField value={inputs.name} placeholder="Name" margin="normal" />
+          )}
+          <TextField
+            value={inputs.email}
+            type={"email"}
+            placeholder="Email"
+            margin="normal"
+          />
+          <TextField
+            value={inputs.password}
+            type={"password"}
+            placeholder="Password"
+            margin="normal"
+          />
+          <Button
+            variant="contained"
+            sx={{ borderRadius: 3, marginTop: 3 }}
+            color="warning"
+          >
+            Submit
+          </Button>
+          <Button
+            onClick={() => setIsSignup(!isSignup)}
+            sx={{ borderRadius: 3, marginTop: 3 }}
+          >
+            Change To {isSignup ? "Login" : "Signup"}
+          </Button>
+        </Box>
+      </form>
+    </div>
+  );
 };
 
 export default Auth;
