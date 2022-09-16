7271eea4c07e8b083cc4e12e69a4bf032ebd9be0
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-16 16:11:02 -0600

    Get all blogs when login

--- frontend/src/components/Blogs.js
+++ frontend/src/components/Blogs.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState();
+  const sendRequest = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/api/blog");
+      const data = await res.data;
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    sendRequest().then((data) => setBlogs(data.blogs));
+  }, []);
   return <div>Blogs</div>;
 };
 
