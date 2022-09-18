3fe70d4b7c29f8343a34d232c3f22ce061b3b046
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-17 18:44:39 -0600

    Update UserBlogs

--- frontend/src/components/UserBlogs.js
+++ frontend/src/components/UserBlogs.js
@@ -1,6 +1,21 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 const UserBlogs = () => {
+  const id = localStorage.getItem("userId");
+
+  const sendRequest = async () => {
+    try {
+      const res = await axios.get(`http://localhost:5000/api/blog/user/${id}`);
+      const data = await res.data;
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    sendRequest();
+  }, []);
   return <div>UserBlogs</div>;
 };
 
