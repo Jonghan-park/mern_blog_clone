baca4d9a74c2f795c8c5e1763b7ad06041dc86bb
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-17 19:44:46 -0600

    Update add blog

--- frontend/src/components/AddBlog.js
+++ frontend/src/components/AddBlog.js
@@ -1,18 +1,50 @@
-import React from "react";
-import { Box, Typography, InputLabel, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Typography, InputLabel, TextField, Button } from "@mui/material";
+import axios from "axios";
 
 const labelStyles = { mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold" };
 const AddBlog = () => {
+  const [inputs, setInputs] = useState({
+    title: "",
+    desc: "",
+    imageURL: "",
+  });
+  const sendRequest = async () => {
+    try {
+      const res = await axios.post("http://localhost:5000/api/blog/add", {
+        title: inputs.title,
+        desc: inputs.desc,
+        image: inputs.imageURL,
+        user: localStorage.getItem("userId"),
+      });
+      const data = await res.data;
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleChange = async (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(inputs);
+    sendRequest().then((data) => console.log(data));
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <Box
           border={3}
-          borderColor="green"
+          borderColor="linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 48%, rgba(0,212,255,1) 100%)"
           borderRadius={10}
           boxShadow="10px 10px 20px #ccc"
           padding={3}
-          margin={3}
+          margin={"auto"}
+          marginTop={3}
           display="flex"
           flexDirection={"column"}
           width={"80%"}
@@ -27,11 +59,37 @@ const AddBlog = () => {
             Post Your Blog
           </Typography>
           <InputLabel sx={labelStyles}>Title</InputLabel>
-          <TextField margin="normal" variant="outlined" />
+          <TextField
+            name="title"
+            onChange={handleChange}
+            value={inputs.title}
+            margin="normal"
+            variant="outlined"
+          />
           <InputLabel sx={labelStyles}>Description</InputLabel>
-          <TextField margin="normal" variant="outlined" />
+          <TextField
+            name="desc"
+            onChange={handleChange}
+            value={inputs.desc}
+            margin="normal"
+            variant="outlined"
+          />
           <InputLabel sx={labelStyles}>ImageURL</InputLabel>
-          <TextField margin="normal" variant="outlined" />
+          <TextField
+            name="imageURL"
+            onChange={handleChange}
+            value={inputs.imageURL}
+            margin="normal"
+            variant="outlined"
+          />
+          <Button
+            sx={{ mt: 2, borderRadius: 4 }}
+            variant="contained"
+            color="warning"
+            type="submit"
+          >
+            Submit
+          </Button>
         </Box>
       </form>
     </div>
